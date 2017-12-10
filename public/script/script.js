8d51d1a55dc8bfe5342b423ba0d8f1e2bea4508f
James Priest <james-priest@outlook.com>
2017-12-10 00:25:33 -0800

    script capturing enter key

--- public/script/script.js
+++ public/script/script.js
@@ -1,17 +1,27 @@
 var myGrid = {
     init: function() {
-        document.querySelector( '#css-code' ).addEventListener( 'keypress', function( e ) {
-            console.log( 'e', e );
-            // console.log( 'a', a );
-            // e( arguments );
-        });
+        var myGrid = this;
+        // document.querySelector( '#css-code' ).addEventListener( 'keypress', function( evt ) {
+        //     myGrid.captureKey( evt );
+        // });
+        document.querySelector( '#css-code' ).onkeypress = function( evt ) {
+            if ( evt.keyCode === 13 || evt.which === 13 ) {
+                myGrid.indentEnterKey();
+                return false;
+            }
+        };
+    },
+    indentEnterKey: function() {
+        console.log( 'indented' );
     },
     captureKey: function( evt ) {
-        console.log('args', evt)
+        // console.log( 'args', evt );
         var myGrid = document.querySelector( '.grid-container' );
         var css = document.querySelector( '#css-code' ).value;
-        console.log( evt );
-        // myGrid.captureKey( evt );
+        // console.log( evt );
+        this.filterKey( evt );
+        // return false;
+        
         var pattern = /([\w-]+:\s*[\w-#\s]+;?$)/gm;
         var cssStatements = '';
         if ( cssStatements = css.match( pattern ) ) {
@@ -19,11 +29,10 @@ var myGrid = {
             myGrid.setAttribute( 'style', cssStatements.join( ' ' ) );
         }
     },
-    filterEnter: function( evt ) {
+    filterKey: function( evt ) {
         if ( evt.keyCode === 13 || evt.which === 13 ) {
+            console.log( "Enter" );
             return false;
-        } else {
-            return true;
         }
     }
 };
