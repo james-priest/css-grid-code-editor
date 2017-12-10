4758572edb078b37e2078d2678a97cab976eb413
James Priest <james-priest@outlook.com>
2017-12-10 03:53:15 -0800

    Update editor with dark theme

--- public/script/script.js
+++ public/script/script.js
@@ -1,80 +1,36 @@
 var myGrid = {
-    // codeEditor: document.querySelector( '.code-editor' ),
-    codeEditor: document.querySelector( '#css-code' ),
-    init: function() {
-        var myGrid = this;
-        myGrid.codeEditor.selectionStart = 20;
-        // document.querySelector( '#css-code' ).addEventListener( 'keypress', function( evt ) {
-        //     myGrid.captureKey( evt );
-        // });
-        // document.querySelector( '.code-editor' ).onkeypress = function( evt ) {
-        myGrid.codeEditor.onkeypress = function( evt ) {
+    gridContainer: document.querySelector( '.grid-container' ),
+    codeEditor: document.querySelector( '.css-code' ),
+    init: function(preFill) {
+        var thisGrid = this;
+        thisGrid.codeEditor.defaultValue = preFill;
+        thisGrid.codeEditor.selectionStart = thisGrid.codeEditor.selectionEnd = 20;
+        thisGrid.codeEditor.oninput = function( evt ) {
+            thisGrid.applyStyle(evt);
+        };
+        thisGrid.codeEditor.onkeypress = function( evt ) {
             var val = this.value;
             if ( evt.keyCode === 13 || evt.which === 13 ) {
-                // myGrid.indentEnterKey('\u000A\u0020\u0020');
-                // myGrid.indentEnterKey('\u03A9');
-
+                var newLineIndent = '\u000A\u0020\u0020';
                 var start, end;
-                if ( typeof this.selectionStart === "number" && typeof this.selectionEnd === "number" ) {
+                if ( typeof this.selectionStart === 'number' && typeof this.selectionEnd === 'number' ) {
                     start = this.selectionStart;
                     end = this.selectionEnd;
-                    this.value = val.slice( 0, start ) + '\u000A\u0020\u0020' + val.slice( end );
+                    this.value = val.slice( 0, start ) + newLineIndent + val.slice( end );
                     
                     // move the caret
                     this.selectionStart = this.selectionEnd = start + 3;
                 }
-
                 return false;
             }
-            myGrid.captureKey( evt );
         };
     },
-    indentEnterKey: function( text ) {
-        console.log( 'indented' );
-        // var sel, range, textNode;
-        // if ( window.getSelection ) {
-        //     sel = window.getSelection();
-        //     if ( sel.getRangeAt && sel.rangeCount ) {
-        //         range = sel.getRangeAt( 0 );
-        //         range.deleteContents();
-        //         textNode = document.createTextNode( text );
-        //         range.insertNode( textNode );
-
-        //         // move caret to end of the newly inserted text node
-        //         range.setStart( textNode, textNode.length );
-        //         range.setEnd( textNode, textNode.length );
-        //         sel.removeAllRanges();
-        //         sel.addRange( range );
-        //     } else if ( document.selection && document.selection.createRange ) {
-        //         range = document.selection.createRange();
-        //         range.pasteHTML( text );
-        //     }
-        // }
-    },
-    captureKey: function( evt ) {
-        // console.log( 'args', evt );
-        var gridContainer = document.querySelector( '.grid-container' );
-        // var css = document.querySelector( '#css-code' ).value;
-        // console.log( this.codeEditor );
-        // var css = this.codeEditor.textContent;
-        var css = this.codeEditor.value;
-        console.log( 'css', css );
-        this.filterKey( evt );
-        // return false;
-        
+    applyStyle: function( evt ) {
+        var css = evt.target.value;
         var pattern = /([\w-]+:\s*[\w-#\s]+;?$)/gm;
-        var cssStatements = '';
-        if ( cssStatements = css.match( pattern ) ) {
-            console.log( 'match', cssStatements );
-            gridContainer.setAttribute( 'style', cssStatements.join( ' ' ) );
-        }
-    },
-    filterKey: function( evt ) {
-        if ( evt.keyCode === 13 || evt.which === 13 ) {
-            console.log( "Enter" );
-            return false;
+        var cssStatements = css.match( pattern );
+        if ( cssStatements ) {
+            this.gridContainer.setAttribute( 'style', cssStatements.join( ' ' ) );
         }
     }
 };
-
-window.onload = myGrid.init();
