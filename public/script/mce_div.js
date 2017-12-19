065a094408aef2b0c841b2370758ad40000f6d94
James Priest <james-priest@outlook.com>
2017-12-19 00:52:46 -0800

    Working code editor with content editable div

--- public/script/mce_div.js
+++ public/script/mce_div.js
@@ -1,10 +1,15 @@
 var myDivCodeEditor = {
     gridContainer: document.querySelector( '.grid-container' ),
     codeEditor: document.querySelector( '.div-code-editor' ),
-    constants: getConstants(),
+    // constants: getConstants(),
     // reConstants: /(?<=:.*)((?<=[\s:])\d+|#[0-9A-Fa-f]{3,6}|\b(solid|dashed|dotted|grid)\b)/gm,
     reConstants: new RegExp('.*:\\s*|.*:|.*?{|[a-z]\\d+|\\w+\\(\\)|(\\d+|#[0-9A-Fa-f]{3,6}|\\b(' + getConstants() + ')(?![\\w-]))', 'gm'),
-    reKeywords: /.*:\s*|.*:|(<\/span>)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax|fr)(?=\W)/gm,
+    // reKeywords: /.*:\s*|.*:|(<\/span>)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax|fr)(?=\W)/gm,
+    reKeywords: new RegExp('.*:\\s*|.*:|(<\\/span>|\\d+)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax|fr)(?=\\W|$)', 'gm'),
+    currentDivLine: '',
+    mouseClick: false,
+    clickElem: '',
+    clickPos: 0,
     init: function(preFill) {
         var thisGrid = this;
         thisGrid.codeEditor.innerHTML = preFill;
@@ -14,56 +19,116 @@ var myDivCodeEditor = {
         // console.log( 'childNodes', this.codeEditor.childNodes[1].childNodes[0] );
         // console.log( 'children', this.codeEditor.children );
         // console.log( thisGrid.constants );
-        console.log( thisGrid.reConstants );
+        // console.log( thisGrid.reConstants );
         
-        thisGrid.setCursor( this.codeEditor.childNodes[ 1 ].childNodes[0], 2 );
+        thisGrid.setCursor( this.codeEditor.childNodes[ 1 ].childNodes[ 0 ], 2 );
+        thisGrid.currentDivLine = thisGrid.getSelectedNode();
 
         thisGrid.codeEditor.oninput = function( evt ) {
-            // filter current word
             // console.log( evt );
-            var selNode, working, newNode;
-            selNode = thisGrid.getSelectedNode();
-            // working = selNode.textContent;
-            working = selNode.innerHTML;
-
-            console.log( 'sel', selNode );
-            console.log( 'textContent', working );
-
-            // keywords
-            // working = working.replace( /(?<=\d)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax|fr|s)(?=\W)/gm, '<span class="mce-keyword">$&</span>' );
-            // constants
-            // working = working.replace( /(?<=:.*)((?<=[\s:])\d+|\#[0-9A-Fa-f]{3,6}|\b(solid|dashed|dotted|grid)\b)/gm, '<span class="mce-constant">$&</span>' );
-            // working = working.replace( /(?<=:.*)(\d+|#[0-9A-Fa-f]{3,6}|\b(solid|dashed|dotted|grid)\b)/gm, '<span class="mce-constant">$&</span>' );
-
-            working = working.replace(thisGrid.reConstants, function(m, group1) {
-                if (group1 !== undefined) {
-                    return '<span class="mce-constant">' + group1 + '</span>';
-                }
-                return m;
-            } );
-            working = working.replace(thisGrid.reKeywords, function(m, group1, group2) {
-                if (group1 !== undefined) {
-                    // console.log(group1 + '<span>' + group2 + '</span>');
-                    return group1 + '<span class="mce-keyword">' + group2 + '</span>';
-                }
-                return m;
-            });
-
-            // TODO: Call a function on the replace & track prevNode & newNode 
+            console.log( '' );
+            var selNode, working, prevValue;
             
-            // if ( working.includes( '<span' ) ) {
-            //     newNode = selNode.childNodes[1]
-            //     // thisGrid.setCursor( newNode.childNodes[0], newNode.childNodes[0].length );
-            // }
+            // selNode = thisGrid.getSelectedNode();
+            selNode = thisGrid.currentDivLine; // containing div of the selected line
+            console.log( 'selNode', selNode.nodeName );
+            console.log( evt );
+
+            working = selNode.textContent;
+            // working = selNode.innerHTML;
+            prevValue = working;
+
+            console.log( '-------------------' );
+            console.log( 'prevValue', prevValue );
+            console.log( 'working', working );
+
+            // DONE: Call a function on string.replace & track prevNode & newNode 
+            if (working.includes(':')) {
+                working = working.replace(thisGrid.reConstants, function(m, group1) {
+                    if (group1 !== undefined) {
+                        return '<span class="mce-constant">' + group1 + '</span>';
+                    }
+                    return m;
+                } );
+                working = working.replace(thisGrid.reKeywords, function(m, group1, group2) {
+                    if (group1 !== undefined) {
+                        return group1 + '<span class="mce-keyword">' + group2 + '</span>';
+                    }
+                    return m;
+                } );
+            }    
+
             selNode.innerHTML = working;
-            // console.log( selNode.lastChild );
-            // console.log( selNode.children );
+
+            console.log( '*** regex match and update innerHTML ***' );
+            console.log( 'prevValue', prevValue );
+            console.log( 'working', working );
+            console.log( '-------------------' );
+            
+            if ( prevValue === working ) {
+                console.log( 'same child' );
+            } else {
+                console.log( 'new children' );
+                selNode.appendChild( document.createTextNode( '' ) );
+            }
+
+            /**
+             * DONE: Set caret position
+             * 1. use current sel.anchor.parent.children.lastChild
+             * 2. length of innerText of last child
+             */
+
+            console.log( '1.', 'sel', selNode );
+            console.log( '2a.', 'selNode.innerHTML', selNode.innerHTML );
+            console.log( '2b.', 'selNode.textContent', selNode.textContent );
+            console.log( '2c.', 'selNode.lastChild', selNode.lastChild );
+            console.log( '2c.', 'selNode.lastChild.nodeType', selNode.lastChild.nodeType );
+            console.log( '2c.', 'selNode.nodeType', selNode.nodeType );
+
+
+            var childNodes = selNode.childNodes;
+            console.log( '3.', 'childNodes', childNodes );
+            // console.log( '4.', 'lastChild', selNode.lastChild );
+            // console.log( '5.', 'lastChild.length', selNode.lastChild.textContent.length );
+            // console.log( '6.', 'window.getSelection().getRangeAt(0)', window.getSelection().getRangeAt(0) );
+            // console.log( '7.', 'window.getSelection()', window.getSelection() );
+            // console.log( '8.', 'window.getSelection().anchorNode', window.getSelection().anchorNode );
+            // console.log( '9.', 'window.getSelection().anchorNode.parentNode', window.getSelection().anchorNode.parentNode );
+           
+            if ( thisGrid.mouseClick ) {
+                thidGrid.mouseClick = false;
+            } else {
+                var curElem = selNode.lastChild;
+                var curPos = 0;
+                if ( selNode.lastChild.nodeType === 3 ) {
+                    curPos = selNode.lastChild.textContent.length;
+                } else {
+                    // curElem = selNode.lastChild;
+                    curPos = selNode.lastChild.outerHTML.length;
+                }
+
+                thisGrid.setCursor( curElem, curPos );
+            }
+            
 
             // set cursor
-            // console.log( selNode.innerHTML.length );
-            // newNode = selNode.childNodes[1]
-            // thisGrid.setCursor( newNode.childNodes[0], newNode.childNodes[0].length );
-            thisGrid.setCursor( selNode.childNodes[0], selNode.childNodes[0].length )
+            // var range = window.getSelection().getRangeAt(0);
+            // var selectedObj = window.getSelection();
+            // var rangeCount = 0;
+            // // var childNodes = selectedObj.anchorNode.parentNode.childNodes;
+            // for (var i = 0; i < childNodes.length; i++) {
+            //     if (childNodes[i] == selectedObj.anchorNode) {
+            //         break;
+            //     }
+            //     if (childNodes[i].outerHTML)
+            //         rangeCount += childNodes[i].outerHTML.length;
+            //     else if (childNodes[i].nodeType == 3) {
+            //         rangeCount += childNodes[i].textContent.length;
+            //     }
+            // }
+            // var curElem = selNode.lastChild;
+            // var curPos = selNode.lastChild.textContent.length;
+            // thisGrid.setCursor(curElem, curPos);
 
             thisGrid.applyStyle(evt);
         };
@@ -76,8 +141,20 @@ var myDivCodeEditor = {
             }
             // <ctrl>+/
             
-            
         };
+        thisGrid.codeEditor.onmouseup = function( evt ) {
+            console.log( 'mouseup true' );
+            thisGrid.mouseup = true;
+            thisGrid.clickElem = thisGrid.getSelectedNode();
+            console.log( '  clickElem:     ', thisGrid.clickElem.nodeName, thisGrid.clickElem.className, '"' + thisGrid.clickElem.textContent + '"');
+            
+            var selNode = thisGrid.currentDivLine; // containing div of the selected line
+            console.log( '  currentDivLine:', selNode.nodeName, selNode.className, '"' + selNode.textContent + '"' );
+        };
+        // DONE: backspace - erase properly
+        // TODO: onmouseup - set parent div for editing (right now its div-code-editor)
+        // TODO: backspace - set proper curPos when deleting current line
+        // TODO: paste - set proper curPos when pasting in a line
     },
     setCursor: function(textNode, pos) {
         var range, sel;
@@ -91,6 +168,8 @@ var myDivCodeEditor = {
         }
     },
     getSelectedNode: function() {
+        // DONE: This changes rather than stay as the div line we are working on.
+        // maybe create a property to track this
         if ( window.getSelection ) {
             var sel = window.getSelection();
             return sel.anchorNode.parentNode;
@@ -115,6 +194,7 @@ var myDivCodeEditor = {
 
         prevNode.parentNode.insertBefore( newNode, prevNode.nextSibling );
         // this.setCursor( textNode, textNode.length );
+        this.currentDivLine = newNode.childNodes[ 0 ].parentNode;        
         this.setCursor( newNode.childNodes[0], 2 );
         // }
         // }
